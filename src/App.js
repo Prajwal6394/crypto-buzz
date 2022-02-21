53e1d040d8763bcbbc70a7f509d8a1bc24c0c2f4
Prajwal Pandey <prajwalpandey2009@gmail.com>
2022-02-21 14:16:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {Switch, Route, Link} from 'react-router-dom';
+import {Layout, Typography, Space} from 'antd';
+import {Navbar, Exchanges, Homepage, CryptoDetails,Cryptocurrencies, News} from './component';
+import './App.css';
+
+const App = () => {
+  return (
+    
+    <div className = "app">
+      <div className = "Navbar">
+        <Navbar/>
+      </div>
+      <div className = "main">
+          <Layout>
+            <div className = "routes">
+              <Switch>
+                <Route exact path='/'>
+                  <Homepage/>
+                </Route>
+                <Route exact path='/exchanges'>
+                  <Exchanges/>
+                </Route>
+                <Route exact path='/cryptocurrencies'>
+                  <Cryptocurrencies/>
+                </Route>
+                <Route exact path='/crypto/:coinID'>
+                  <CryptoDetails/>
+                </Route>
+                <Route exact path='/news'>
+                  <News/>
+                </Route>
+              </Switch>
+            </div>
+          </Layout>
+      </div>
+      <div className = "footer" >
+
+      </div>
+    </div>
+  )
+}
+
+
+
+export default App
